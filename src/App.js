1b4342d86818610f24bf48045b6202af765d9195
lee951109 <sk985326@naver.com>
2022-07-05 11:30:38 +0900

    Weather APP

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+
+import { useEffect } from 'react';
 import './App.css';
+import WeatherBox from './components/WeatherBox';
 
 function App() {
+
+  const apiKey = "efd88f078d10b3680f1707a27ec361c0";
+
+  const getCurrentLocation = () => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      let lat = position.coords.latitude;
+      let lon = position.coords.longitude;
+
+      getWeatherByCurrentLocation(lat, lon);
+    })
+  }
+
+  const getWeatherByCurrentLocation = async(lat, lon) => {
+    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+    let response = await fetch(url);
+    let data = await response.json();
+    console.log("data" , data);
+  }
+
+
+  useEffect(() => {
+    getCurrentLocation();
+
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <WeatherBox />
     </div>
   );
 }
---
+++ src/components/WeatherBox.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const WeatherBox = () => {
+  return (
+    <div>
+      <div>신풍</div>
+      <h2>30도 / 230 화씨</h2>
+      <h3>맑은 하능</h3>
+      
+    </div>
+  )
+};
+
+export default WeatherBox
