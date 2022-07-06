b23d2a2bb0146a587c5b292c17cfdf1ae55aacc3
lee951109 <sk985326@naver.com>
2022-07-06 10:21:46 +0900

    spinner

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import ClipLoader from "react-spinners/ClipLoader";
 
 import WeatherBox from './components/WeatherBox';
 import WeatherButton from './components/WeatherButton';
@@ -11,7 +12,9 @@ function App() {
   const apiKey = "efd88f078d10b3680f1707a27ec361c0";
   const [weather, setWeather] = useState(null);
   const [city, setCity] = useState("");
+  const [loading, setLoading] = useState(false);
   const cities = ['paris', 'london', 'tokyo', 'ansan'];
+
 
   const getCurrentLocation = () => {
     navigator.geolocation.getCurrentPosition((position) => {
@@ -25,19 +28,23 @@ function App() {
   // 현재 위치의 날씨
   const getWeatherByCurrentLocation = async(lat, lon) => {
     let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+    setLoading(true);
     let response = await fetch(url);
     let data = await response.json();
 
     setWeather(data);
+    setLoading(false);
   }
 
   // 바튼을 클릭 했을 시, 나라별 api
   const getWeatherByCity = async() => {
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    setLoading(true);
     let res = await fetch(url);
     let data = await res.json();
     console.log("data? ", data);
     setWeather(data);
+    setLoading(false);
   }
 
   useEffect(() => {
@@ -50,10 +57,21 @@ function App() {
 
   return (
     <div>
-      <div className='container'>
-        <WeatherBox weather={weather}/>
-        <WeatherButton cities={cities} setCity={setCity}/>
-      </div>
+      {loading ? 
+      (
+        <div className='container'>
+          <ClipLoader color="#f88c6b" loading={loading} size={150} />
+        </div>
+      )
+      : 
+      (
+        <div className='container'>
+          <ClipLoader color="#f88c6b" loading={loading} size={150} />
+          <WeatherBox weather={weather}/>
+          <WeatherButton cities={cities} setCity={setCity}/>
+        </div>
+      )
+      }
     </div>
   );
 }
